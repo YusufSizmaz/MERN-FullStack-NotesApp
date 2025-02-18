78f5153effb97ab8a64efd9316e912b4de0701da
YusufSizmaz <yusuf.sizmaz11@gmail..com>
2025-02-18 15:24:56 +0300

    password and email controller

--- FrontEnd/src/pages/Login/Login.jsx
+++ FrontEnd/src/pages/Login/Login.jsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import { Link } from "react-router-dom";
 import PasswordInput from "../../components/input/Passwordinput";
+import { validateEmail } from "../../utilis/helper";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+    if (!password) {
+      setError("Please enter the password.");
+      return;
+    }
+
+    setError("");
+
+    // Login API Call
+  };
+
   return (
     <>
       <Navbar />
       <div className="flex items-center justify-center mt-28">
         <div className="w-96 border border-gray-300 rounded bg-white px-7 py-10">
-          <form onSubmit={() => {}}>
+          <form onSubmit={handleLogin}>
             <h4 className="text-2xl mb-7">Login</h4>
 
             <input
               type="text"
               placeholder="Email"
               className="w-full text-sm bg-transparent px-5 py-3 rounded mb-5 border border-gray-300 focus:border-gray-400 outline-none"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
 
-            <PasswordInput />
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
 
             <button
               type="submit"
