39dd968bea503fdd09e398856d2718cac5c01a0e
YusufSizmaz <yusuf.sizmaz11@gmail..com>
2025-02-19 10:03:13 +0300

    serachBar add

--- FrontEnd/src/components/Cards/ProfileInfo.jsx
+++ FrontEnd/src/components/Cards/ProfileInfo.jsx
@@ -5,7 +5,7 @@ const ProfileInfo = ({ onLogout }) => {
   return (
     <div className="flex items-center gap-3">
       <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-200">
-        {getInitials("Yusuf Sızmaz")}
+        {getInitials("Yusuf Can Sızmaz ")}
       </div>
 
       <div>
--- FrontEnd/src/components/Navbar/Navbar.jsx
+++ FrontEnd/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ProfileInfo from "../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const navigate = useNavigate();
 
   const onLogout = () => {
     navigate("/login");
   };
 
+  const handleSearch = () => {};
+  const onClearSerach = () => {
+    setSearchQuery("");
+  };
+
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
 
-      <SearchBar />
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSerach={onClearSerach}
+      />
 
       <ProfileInfo onLogout={onLogout} />
     </div>
