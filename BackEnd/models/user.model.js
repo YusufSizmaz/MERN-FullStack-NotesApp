893209ea6004f6d0f1d397924bfd19c6f25ee76f
YusufSizmaz <yusuf.sizmaz11@gmail..com>
2025-02-26 14:37:50 +0300

    bcrypt hash

--- BackEnd/models/user.model.js
+++ BackEnd/models/user.model.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   fullName: { type: String },
   email: { type: String },
-  password: { type: String },
+  password: { type: String, required: true },
   createdOn: { type: Date, default: new Date().getTime() },
+});
+
+// Şifreyi kaydetmeden önce hash'le
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
